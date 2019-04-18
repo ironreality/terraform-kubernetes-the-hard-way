c5230f73d0f1fdc0f692fc102866744adbdfe031
Vadym Surhyk <vsurzhyk@mirantis.com>
2019-04-18 11:47:55 +0300

    fix api-to-kubelet rbac settings

--- control-plane/04-create-api-kubelet-rbac.sh
+++ control-plane/04-create-api-kubelet-rbac.sh
@@ -5,8 +5,10 @@ set -o nounset
 set -o pipefail
 
 echo
-echo "Generating api to kubelet RBAC..."
+echo "Generating api to kubelet RBAC configs..."
 
+echo
+echo "Generating api to kubelet cluster role..."
 cat > kube-apiserver-to-kubelet.yaml<<EOF
 apiVersion: rbac.authorization.k8s.io/v1beta1
 kind: ClusterRole
@@ -29,10 +31,31 @@ rules:
       - "*"
 EOF
 
-echo
-echo "Copying the RBAC config to controller-0..."
-gcloud compute scp kube-apiserver-to-kubelet.yaml controller-0:~/ || { echo "Can't copy the files to ${instance}! Exiting..."; exit 1; }
+echo "Generating api to kubelet cluster role binding..."
+cat > kube-apiserver-to-kubelet-binding.yaml<<EOF
+apiVersion: rbac.authorization.k8s.io/v1beta1
+kind: ClusterRoleBinding
+metadata:
+  name: system:kube-apiserver
+  namespace: ""
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: system:kube-apiserver-to-kubelet
+subjects:
+  - apiGroup: rbac.authorization.k8s.io
+    kind: User
+    name: kubernetes
+EOF
 
 echo
-echo "Applying the RBAC config..."
-gcloud compute ssh controller-0 -- sudo kubectl apply --kubeconfig admin.kubeconfig -f kube-apiserver-to-kubelet.yaml || { echo "Can't create the RBAC policy! Exiting..."; exit 1; }
+echo "Copying the RBAC config to controller-0..."
+gcloud compute scp kube-apiserver-to-kubelet.yaml kube-apiserver-to-kubelet-binding.yaml controller-0:~/ || { echo "Can't copy the files to controller-0! Exiting..."; exit 1; }
+
+echo
+echo "Creating the cluster role..."
+gcloud compute ssh controller-0 -- sudo kubectl apply --kubeconfig admin.kubeconfig -f kube-apiserver-to-kubelet.yaml || { echo "Can't create the cluster role! Exiting..."; exit 1; }
+
+echo
+echo "Creating the cluster role binding..."
+gcloud compute ssh controller-0 -- sudo kubectl apply --kubeconfig admin.kubeconfig -f kube-apiserver-to-kubelet-binding.yaml || { echo "Can't create the cluster role binding! Exiting..."; exit 1; }
